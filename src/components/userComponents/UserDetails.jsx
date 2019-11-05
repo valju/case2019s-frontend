2a7a1c8dc744790e69701e934b91e59c11a09280
tonyastro77 <tonyastro77@users.noreply.github.com>
2019-11-05 13:57:54 +0200

    Fixing bugs from files not found to make router work

---
+++ src/components/userComponents/UserDetails.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import UserItem from './UserItem';
+import { getUser } from '../../actions/users';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+
+class UserDetails extends Component {
+  componentWillMount() {
+    this.props.userGetById(this.props.id);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.users.userCurrent === null ? (
+          <p>Waiting server response. Activity indicator could go here.</p>
+        ) : (
+            <div>
+              <UserItem item={this.props.users.userCurrent} />
+              <Link to={`/user/update/${this.props.users.userCurrent.id}`}>Update</Link>
+            </div>
+          )}
+
+        <p>
+          <Link to='/users'>Back</Link>
+        </p>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  userGetById: (id) => {
+    dispatch(getUser(id));
+  }
+});
+
+const mapStateToProps = state => ({
+  users: state.users,
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(UserDetails);
+
