03058fd8cc19f6124fab104f5c5f7b383aac9d36
vehuo <vesa.huotarinen@gmail.com>
2019-11-07 11:50:16 +0200

    Added EventType routing. Fixed app.js navigation.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./App.css";
 
 import Navigator from './router/Navigator';
-import Users from "./views/userViews/Users";
-import EventTypes from "./views/eventTypeViews/EventTypes";
 
 
 function App() {
@@ -11,8 +9,6 @@ function App() {
     <div className="App">
       <h1>Testing</h1>
       <Navigator />
-      <Users />
-      <EventTypes />
     </div>
   );
 }
