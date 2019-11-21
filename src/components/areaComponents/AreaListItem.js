b0ffa21b3dfe7796adc9ae3a1081c047089d6331
BlerimPlakolli <36608374+BlerimPlakolli@users.noreply.github.com>
2019-11-21 17:07:00 +0200

    Update AreaListItem.js

--- src/components/areaComponents/AreaListItem.js
+++ src/components/areaComponents/AreaListItem.js
@@ -8,7 +8,6 @@ export default class AreaListItem extends Component {
     return (
       <li>
         <AreaItem item={this.props.item} />
-
         &nbsp;&nbsp;
         <Link to={`/area/${this.props.item.id}`}>View</Link>
         &nbsp;&nbsp;
