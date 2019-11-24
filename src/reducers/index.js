518b2499f269f152aff86d27e25b9654ab45a6e1
Yoseph <jofinhel@gmail.com>
2019-11-24 08:04:24 +0200

    added .idea folder in ignore, areauser now showing correct values

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,9 @@ import { combineReducers } from "redux";
 
 import users from "./users";
 import eventTypes from "./eventTypes";
-import urgencyTypes from './urgencyTypes';
+import urgencyTypes from "./urgencyTypes";
 import locationTypes from "./locationTypes";
-import areas from './areas';
+import areas from "./areas";
 import locations from "./locations";
 import areaUsers from "./areaUsers";
 const rootReducers = () =>
